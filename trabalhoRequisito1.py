570cb854daabb5027a8789141aef8a6d6d4cb502
Unknown <luizfernandovieira_cf@hotmail.com>
2018-05-11 22:34:52 -0300

    update requisito1

--- trabalhoRequisito1.py
+++ trabalhoRequisito1.py
@@ -66,7 +66,6 @@ centers[0, 1] = avg_skin_v
 centers[1, 0] = avg_no_skin_u
 centers[1, 1] = avg_no_skin_v
 
-# criteria = (cv2.TERM_CRITERIA_ESP, 10, 1.0)
 criteria = (cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 flags = cv2.KMEANS_USE_INITIAL_LABELS
 compactness, labels, centers = cv2.kmeans(np.array(z, np.float32), 2, None, criteria, 10, flags, centers)
@@ -89,5 +88,13 @@ for i in range(0, im278_yuv.shape[0]):
     for j in range(0, im278_yuv.shape[1]):
         im278_bw[i, j] = is_skin_color(im278_yuv[i, j], centers)
 
-cv2.imwrite("SkinDataset/GT_bw/243.jpg", im243_bw)
-cv2.imwrite("SkinDataset/GT_bw/278.jpg", im278_bw)
+cv2.imwrite("SkinDataset/GT_bw_results/243.jpg", im243_bw)
+cv2.imwrite("SkinDataset/GT_bw_results/278.jpg", im278_bw)
+
+im_gray = cv2.imread("SkinDataset/GT/243.jpg", 0)
+(thresh, im_bw) = cv2.threshold(im_gray, 8, 255, cv2.THRESH_BINARY)
+cv2.imwrite("SkinDataset/GT_bw/243.jpg", im_bw)
+
+im_gray = cv2.imread("SkinDataset/GT/278.jpg", 0)
+(thresh, im_bw) = cv2.threshold(im_gray, 8, 255, cv2.THRESH_BINARY)
+cv2.imwrite("SkinDataset/GT_bw/278.jpg", im_bw)
